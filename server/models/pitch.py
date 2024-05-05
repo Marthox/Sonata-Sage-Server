c38688b233b93f119ea02f296aceac1dac5cd38a
MarthoxGJ <43354689+MarthoxGJ@users.noreply.github.com>
2024-05-04 19:54:19 -0500

    Add return type to computed file decorator

--- server/models/pitch.py
+++ server/models/pitch.py
@@ -6,7 +6,7 @@ from constants.music import CONCERT_PITCH
 class Pitch(BaseModel):
     frequency: float = Field(CONCERT_PITCH, ge=0.0)
 
-    @computed_field
+    @computed_field(return_type=int)
     def id(self):
         return int(12 * (math.log2(self.frequency / CONCERT_PITCH)))
 
